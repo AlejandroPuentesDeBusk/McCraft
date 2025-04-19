a441d9876ccf4033dc71ba1c6cc440de66877508
Alejandro Puentes de busk <alejandro@192.168.1.16>
2025-04-19 01:49:57 -0600

    update, delete, create, get, ingredient

--- Backend/products/src/routes/routes_product.js
+++ Backend/products/src/routes/routes_product.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import { get_ingredients, get_ingredient_by_id, delete_ingredient, create_ingredient, update_ingredient } from '../controllers/ingredients_controllers.js';
 
 
 const router = express.Router();
 
+
+//ingredients____________________________________________________
+router.get('/ingredients',get_ingredients);
+router.get('/ingredient/:id', get_ingredient_by_id);
+
+router.post('/ingredients/create', create_ingredient);
+router.put('/ingredients/update/:id', update_ingredient);
+router.delete('/ingredients/delete/:id', delete_ingredient);
+
+
+//products____________________________________________________-
 
 
 export default router;
