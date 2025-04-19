23d0b69676385b8c4538b63e1d6d2288c2f1b6d2
Alejandro Puentes de busk <alejandro@192.168.1.16>
2025-04-19 10:36:06 -0600

    routes_ combo_products

--- Backend/products/src/routes/routes_product.js
+++ Backend/products/src/routes/routes_product.js
@@ -5,6 +5,9 @@ import { get_products, get_product_by_id, delete_product, update_product, create
 
 import { list_all, list_by_product, add_ingredient, update_ingredient, remove_ingredient } from '../controllers/associations_tables_controllers/product_ingredients_controllers.js';
 
+import { get_combos, get_combo_by_id, delete_combo, update_combo, create_combo, get_combo_full } from '../controllers/combo_controllers.js';
+
+import { list_by_combo, add_product, remove_product, update_quantity, list_all_combos } from '../controllers/associations_tables_controllers/combo_product_controller.js';
 
 const router = express.Router();
 
@@ -48,7 +51,24 @@ router.put('/product/ingredient/update/:id/:ingredientId', update_ingredient);
 router.delete('/product/ingredient/remove/:id/:ingredientId', remove_ingredient);
 
 
+//________________________________Combos__________________
 
+router.get('/combos',              get_combos);           // GET  /combos
+router.get('/combo/:id',           get_combo_by_id);      // GET  /combo/123
+router.get('/combos/full/:id', get_combo_full);
+router.post('/combos/create',      create_combo);         // POST /combos/create
+router.put('/combos/update/:id',   update_combo);         // PUT  /combos/update/123
+router.delete('/combos/delete/:id', delete_combo);        
+
+
+//_____________combo_products___________________________-
+
+
+router.get('/combos/products',list_all_combos);
+router.get('/combos/products/:comboId',list_by_combo);
+router.post('/combos/products/add/:comboId',add_product);
+router.put('/combos/products/update/:comboId/:productId',update_quantity);
+router.delete('/combos/products/remove/:comboId/:productId',remove_product);
 
 
 
